148a841d457a85e8f08e3b438304be1449f66a13
danielfelipeluborealpe@gmail.com <danielfelipeluborealpe@gmail.com>
2021-12-05 20:34:01 -0500

    Se agregan aportes de la estructura de las colecciones

--- ProyectoFinalMinTics/Backend/src/models/Proyecto.js
+++ ProyectoFinalMinTics/Backend/src/models/Proyecto.js
@@ -26,5 +26,5 @@ const proyectoShema = new Schema({
     timestamps: true
 });
 
-//Mongo crea por debajo una coleccion lalmada usuarios
+//Mongo crea por debajo una coleccion llamada usuarios
 module.exports = model('Proyecto',proyectoShema);
